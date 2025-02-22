926098df81a9087feb5ac2bf46076d49aa974ada
dukpe <daniellamodukpe@gmail.com>
2025-02-22 22:55:02 +0100

    feat: updating app_url

--- app/integration_info.py
+++ app/integration_info.py
@@ -76,7 +76,7 @@ integration_data = {
                 "options": ["User-1", "User-2", "User-3", "User-4"]  
             }  
         ],  
-        "tick_url": "https://dukpe.pythonanywhere.com/tick",  
+        "tick_url": "https://hngx-stage3-telex-integration-linkedin.vercel.app/tick",  
         "target_url": ""  
     }  
 }
